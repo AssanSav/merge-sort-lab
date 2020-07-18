39b9408b26677619550d3d0121db12feb5b65165
AssanSav <assane.savadogo81@gmail.com>
2020-07-18 17:57:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+function findMinAndRemoveSorted(array) {
+  let min = array[0]
+  let minIndex = 0
+  for (let i = 0; i < array.length; ++i) {
+    if (array[i] < min) {
+      min = array[i]
+      minIndex = i
+    }
+  }
+  array.splice(minIndex, 1)
+  return min
+}
 
+function merge(firstArray, secondArray) {
+  let sorted = []
+  while (firstArray.length != 0 && secondArray.length != 0) { 
+    if (firstArray[0] < secondArray[0]) {
+      sorted.push(findMinAndRemoveSorted(firstArray))
+    }
+    else {
+      sorted.push(findMinAndRemoveSorted(secondArray))
+    }
+  }
+  return sorted.concat(firstArray).concat(secondArray)
+}
+
+function mergeSort(array) {
+  let midPoint = array.length / 2
+  let firstHalf = array.slice(0, midPoint)
+  let secondHalf = array.slice(midPoint, array.length)
+  if (array.length < 2) {
+    return array
+  }
+  else {
+    return merge(mergeSort(firstHalf), mergeSort(secondHalf))
+  }
+}
