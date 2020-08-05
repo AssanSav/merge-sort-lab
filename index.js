c4039d620294c327ba89de667314a64025445adb
AssanSav <assane.savadogo81@gmail.com>
2020-08-05 17:37:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 function findMinAndRemoveSorted(array) {
-  let min = array[0]
-  let minIndex = 0
-  for (let i = 0; i < array.length; ++i) {
+  let min = array[0];
+  let minIndex = 0;
+  for (let i = 0; i < array.length; i++) {
     if (array[i] < min) {
-      min = array[i]
-      minIndex = i
+      min = array[i];
+      minIndex = i;
     }
   }
-  array.splice(minIndex, 1)
-  return min
+  array.splice(minIndex, 1);
+  return min;
 }
 
 function merge(firstArray, secondArray) {
-  let sorted = []
-  while (firstArray.length != 0 && secondArray.length != 0) { 
+  let sorted = [];
+  while (firstArray.length != 0 && secondArray.length != 0) {
     if (firstArray[0] < secondArray[0]) {
-      sorted.push(findMinAndRemoveSorted(firstArray))
-    }
-    else {
-      sorted.push(findMinAndRemoveSorted(secondArray))
+      sorted.push(findMinAndRemoveSorted(firstArray));
+    } else {
+      sorted.push(findMinAndRemoveSorted(secondArray));
     }
   }
-  return sorted.concat(firstArray).concat(secondArray)
+  return sorted.concat(firstArray).concat(secondArray);
 }
 
 function mergeSort(array) {
@@ -30,8 +29,7 @@ function mergeSort(array) {
   let secondHalf = array.slice(midPoint, array.length)
   if (array.length < 2) {
     return array
-  }
-  else {
+  } else {
     return merge(mergeSort(firstHalf), mergeSort(secondHalf))
   }
 }
